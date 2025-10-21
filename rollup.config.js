e1cdb075aa8df13722ad3fc8e461764a6b183334
Blake Moore <blake@blakemoore.io>
2025-10-20 21:53:34 -0400

    More package publishing fixes

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import svgr from '@svgr/rollup';
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 import { readFileSync } from 'fs';
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
@@ -29,9 +31,10 @@ export default {
     resolve(),
     commonjs(),
     postcss({
-      config: {
-        path: './postcss.config.cjs',
-      },
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
       extensions: ['.css'],
       minimize: true,
       inject: false,
